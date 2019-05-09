a9af71da708f1e0b85a76593fa474b6ede91eabb
Park Mion-ger <33119492+Mionger@users.noreply.github.com>
2019-05-10 00:06:45 +0800

    Add files via upload
    Update ADD/ADDU/SUB/SUBU/AND/OR/XOR/NOR/SLT/SLTU/SLL/SRL/SRA/SLL/SRL/SRAV/JR

--- dasm_code.py
+++ dasm_code.py
@@ -1,4 +1,6 @@
-from dasm_tool import *
+from dasm_tool import hex_to_bin
+from dasm_tool import bin_to_hex
+from dasm_tool import addr_bin_to_hex
 
 dict_reg = {
     "00000" : "$0 ",
@@ -53,6 +55,9 @@ class Code(object):
         func = self.machine_code_bin[26:32]
         imme = self.machine_code_bin[16:32]
         addr = self.machine_code_bin[6:32]
+        
+        if op == "000000":
+            self.mips_code = self.op_0000(rs, rt, rd, sa, func)
         if op == "001000":
             self.mips_code = self.addi(rs, rt, imme)
         if op == "001001":
@@ -83,8 +88,42 @@ class Code(object):
         if op == "000011":
             self.mips_code = self.jal(addr)
 
-    def op_0000(self):
-        return
+    def op_0000(self, rs:'str', rt:'str', rd:'str', shamt:'str', func:'str'):
+        # 顺序为dst
+        if func == "100000":
+            return ("add     "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100001":
+            return ("addu    "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100010":
+            return ("sub     "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100011":
+            return ("subu    "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100100":
+            return ("and     "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100101":
+            return ("or      "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100110":
+            return ("xor     "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "100111":
+            return ("nor     "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "101010":
+            return ("slt     "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "101011":
+            return ("sltu    "+dict_reg[rd]+","+dict_reg[rs]+","+dict_reg[rt])
+        if func == "000100":
+            return ("sllv    "+dict_reg[rd]+","+dict_reg[rt]+","+dict_reg[rs])
+        if func== "000110":
+            return ("srlv    "+dict_reg[rd]+","+dict_reg[rt]+","+dict_reg[rs])
+        if func== "000111":
+            return ("srav    "+dict_reg[rd]+","+dict_reg[rt]+","+dict_reg[rs])
+        if func == "000000":
+            return ("sll     "+dict_reg[rd]+","+dict_reg[rt]+","+bin_to_hex(shamt, 32, 0))
+        if func == "000010":
+            return ("srl     "+dict_reg[rd]+","+dict_reg[rt]+","+bin_to_hex(shamt, 32, 0))
+        if func == "000011":
+            return ("sra     "+dict_reg[rd]+","+dict_reg[rt]+","+bin_to_hex(shamt, 32, 0))
+        if func == "001000":
+            return ("jr      "+dict_reg[rs])
 
     def addi(self, rs:'str', rt:'str', imme:'str'):
         return ("addi    "+dict_reg[rs]+","+dict_reg[rt]+","+bin_to_hex(imme, 32, 1))
@@ -127,8 +166,3 @@ class Code(object):
 
     def jal(self, addr:'str'):
         return ("jal     "+addr_bin_to_hex(addr))
-# debug for __init__(str)
-# c = Code("01234567\n")
-# # c.deal_input()
-# for i in range(len(c.machine_code_hex)):
-#     print(c.machine_code_hex[i])
